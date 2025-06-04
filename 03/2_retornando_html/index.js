1b46feb52bb54e35201bf69dc4a6fa56c4ccc598
Gabrielosil <gabriel.osilva59@senacsp.edu.br>
2025-06-03 21:36:58 -0300

    Feat: Atualização

---
+++ 03/2_retornando_html/index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const port = 3001;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><body><h1>Olá mundo!</h1></body></html>');
+    res.end();
+});
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
